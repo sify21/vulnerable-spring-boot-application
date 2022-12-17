abccc43a4b259e11edb99453cc2824e7372dc490
司芳源 <sify1221@gmail.com>
2022-12-18 00:24:41 +0800

    controller interface

---
+++ src/main/java/com/contrastsecurity/demo/providersearch/ProviderController2.java
@@ -0,0 +1,37 @@
+package com.contrastsecurity.demo.providersearch;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+// interface based controller必须有@Controller注解
+@Controller
+public class ProviderController2 implements ProviderControllerInterface {
+	@Value("${spring.application.name}")
+	String appName;
+
+	@Autowired
+	ProviderSearchDAO providerSearchDAO;
+
+	@Override
+	@GetMapping("/hehe")
+	public String index(Model model) {
+		model.addAttribute("appName", appName);
+		model.addAttribute("noSearch", true);
+		return "index";
+	}
+
+	@Override
+	@PostMapping("/hehe")
+	public String search(Map<String, String> body, Model model) {
+		model.addAttribute("zipCode", body.get("zipCode"));
+		List<Object[]> results = providerSearchDAO.getProvidersInZipCode(body.get("zipCode"));
+		model.addAttribute("results", results);
+		return "index";
+	}
+}
